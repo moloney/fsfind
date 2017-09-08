e9931ca275f9ff4c086babc9bae37eaf88ce6021
Brendan Moloney <moloney@ohsu.edu>
2017-09-07 21:52:38 -0700

    ENH+RF: Improve packaging and prep for upload to pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,91 @@
-import sys
-from setuptools import setup
+import os, sys, io
+from setuptools import setup, Command
+from shutil import rmtree
 
+# Package meta-data.
+NAME = 'fsfind'
+DESCRIPTION = 'Efficiently find files and directories'
+URL = 'https://github.com/moloney/fsfind'
+EMAIL = 'moloney.brendan@gmail.com'
+AUTHOR = 'Brendan Moloney'
+VERSION = '0.1dev'
+
+# Dependencies
 if sys.version_info < (3,):
-    install_requires = ['scandir']
+    REQUIRED = ['scandir']
+else:
+    REQUIRED = []
 
-setup(name='fsfind',
-      version='0.1dev',
-      description='Find files and directories',
-      author='Brendan Moloney',
-      author_email='moloney@ohsu.edu',
-      install_requires=install_requires,
-      extras_require = {'test': ["nose"]},
-      py_modules=['fsfind'],
-      test_suite = 'nose.collector'
-     )
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+
+class PublishCommand(Command):
+    """Support setup.py publish."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    py_modules=['fsfind'],
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'publish': PublishCommand,
+    },
+)
